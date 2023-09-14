0c0dae42c298b751f91cfcedfd4df4dfb1f42472
Theoreticallyhugo <hugo@raballo.de>
2023-09-15 00:00:25 +0200

    incorporated angelinas feedback for wordle.py

--- wordle.py
+++ wordle.py
@@ -11,6 +11,7 @@ creating a list of instances, to iterate through.
 
 class Wordle:
     def __init__(self, target_word: str) -> None:
+        """this class provides the logic and datastructures for a wordle game"""
         # the word that is to be guessed
         self.target_word = target_word
         # list of previous guesses
@@ -26,10 +27,21 @@ class Wordle:
         self.matched = False
 
     def find_match(self, try_word):
-        """
+        """find matches in the letters of the guessed word, return correctness
+
+        method compares each letter of the current try to each letter of the
+        target word. whether and how well a letter matches is encoded with the
+        following integers:
         1: not_included
         2: wrong_place
         3: right_place
+        if the target word is guessed, it will be noted in member variable:
+            self.matched
+
+        args:
+            try_word: str of the current try
+        returns: list of five int. each int corresponds to the letter pair with
+            the same index
         """
         # if its the right word, skip all the matching process
         if try_word == self.target_word:
@@ -54,6 +66,9 @@ class Wordle:
         return matches
 
     def add_new_try(self, try_word):
+        """call all methods that are needed to add a new try to the object
+        save some compute by forgoing the analysis, if the word has been matched
+        """
         # if this wordles word has been matched before, skip the adding process
         if self.matched:
             return
